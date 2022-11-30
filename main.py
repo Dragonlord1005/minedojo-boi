0af59fc8e844654671297c98fdc3a99c4256c532
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-11-30 23:23:46 +0000

    Ugh

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ env = minedojo.make(task_id="harvest_milk", image_size=(160, 256))
 
 obs = env.reset()
 
-next_obs, reward, done, info = env.step(action)
+
+for i in range(10):
+    act = env.action_space.no_op()
+    act[0] = 1
+    if i % 10 == 0:
+        act[2] = 1
+    next_obs, reward, done, info = env.step(act)
+
+env.close()
